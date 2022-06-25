2af3ced9546db94e47fdd93aeb99c10086d76447
YanaShliapina <anton.larchenko@itechcraft.com>
2022-06-25 15:16:58 +0300

    Create dream view and controller.

---
+++ SpendMoney/SpendMoney/ViewModels/CreateDreamViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SpendMoney.ViewModels
+{
+    public class CreateDreamViewModel
+    {
+        public string Name { get; set; }
+        public int SaveType { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public decimal TargetAmount { get; set; }
+        public decimal CurrentAmount { get; set; }
+    }
+}
